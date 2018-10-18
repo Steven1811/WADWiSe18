b3c75e6721a17d29ccfd1f005213b9258085c74f
Steven1811 <s0553035@htw-berlin.de>
2018-10-18 15:13:02 +0200

    Initial Commit

---
+++ findmefinder/src/main/java/de/htw/ai/wad/findmefinder/FindMe.java
@@ -0,0 +1,13 @@
+package de.htw.ai.wad.findmefinder;
+
+/**
+ * Hello world!
+ *
+ */
+public class FindMe
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
