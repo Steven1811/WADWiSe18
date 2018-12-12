933f0465591f670846b43f12be7a2ef6a5e5c49a
Steven1811 <s0553035@htw-berlin.de>
2018-12-12 23:04:55 +0100

    Added conditional Webpack plugin loading

--- AddressViz/frontend/vue.config.js
+++ AddressViz/frontend/vue.config.js
@@ -2,30 +2,31 @@
 const { WebpackWarPlugin } = require('webpack-war-plugin');
 
 module.exports = {
-    // proxy all webpack dev-server requests starting with /api
-    // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
-    // see https://cli.vuejs.org/config/#devserver-proxy
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:8088',
-          ws: true,
-          changeOrigin: true
-        }
+  // proxy all webpack dev-server requests starting with /api
+  // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
+  // see https://cli.vuejs.org/config/#devserver-proxy
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088',
+        ws: true,
+        changeOrigin: true
       }
-    },
-    // Change build paths to make them Maven compatible
-    // see https://cli.vuejs.org/config/
-    outputDir: 'target/dist',
-    assetsDir: 'static',
-    baseUrl: '/advi/',
+    }
+  },
+  // Change build paths to make them Maven compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'target/dist',
+  assetsDir: 'static',
+  baseUrl: '/advi/',
 
-    configureWebpack: {
-      plugins: [
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
         new WebpackWarPlugin({
           archiveName: "advi",
           webInf: "./WEB-INF"
-        })
-      ]
+        }))
     }
   }
+}
