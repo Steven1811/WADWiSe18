a6a01527100a5945b23f2ae6c8254bffb37517f7
Steven1811 <s0553035@htw-berlin.de>
2018-12-18 11:50:34 +0100

    Completed Edit Contact function

--- AddressViz/frontend/src/components/Main.vue
+++ AddressViz/frontend/src/components/Main.vue
@@ -2,15 +2,15 @@
   <v-container fluid grid-list-md fill-height>
     <v-layout column >
         <v-flex d-flex xs1 sm1 md1>
+            <!-- EditContact Dialog !-->
             <v-card color="indigo">
-                <v-dialog v-model="addContactDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-                    <v-btn slot="activator" color="orange">Add</v-btn>
+                <v-dialog v-model="editContactDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
                     <v-card>
                         <v-toolbar dark color="primary">
-                            <v-btn icon dark @click="addContactDialog = false">
+                            <v-btn icon dark @click="editContactDialog = false">
                                 <v-icon>close</v-icon>
                             </v-btn>
-                            <v-toolbar-title>Add Contact</v-toolbar-title>
+                            <v-toolbar-title>Edit Contact</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                             <v-flex d-flex xs6 sm6 md4>
@@ -39,7 +39,66 @@
                                         label="Private"
                                         v-model="contactDialogData.private"
                                     ></v-checkbox>
-                                    <v-btn @click="addContactDialog = false">Save</v-btn>
+                                    <v-divider></v-divider>
+                                    <v-text-field 
+                                        label="Longitude"
+                                        v-model="contactDialogData.longitude"
+                                        disabled
+                                    ></v-text-field>
+                                    <v-text-field 
+                                        label="Latitude"
+                                        v-model="contactDialogData.latitude"
+                                        disabled
+                                    ></v-text-field>
+                                    <v-divider></v-divider>
+                                    <v-btn @click="updateContact(contactDialogData)">Update</v-btn>
+                                    <v-btn @click="deleteContact(contactDialogData)">Delete</v-btn>
+                                </v-form>
+                            </v-flex>
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
+            </v-card>
+
+            <!-- AddContact Dialog !-->
+            <v-card color="indigo">
+                <v-dialog v-model="addContactDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+                    <v-btn slot="activator" color="orange">Add</v-btn>
+                    <v-card>
+                        <v-toolbar dark color="primary">
+                            <v-btn icon dark @click="addContactDialog = false; addContactData = {}">
+                                <v-icon>close</v-icon>
+                            </v-btn>
+                            <v-toolbar-title>Add Contact</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-flex d-flex xs6 sm6 md4>
+                                <v-form>
+                                    <v-text-field
+                                        label="Full name"
+                                        v-model="addContactData.name"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="Address"
+                                        v-model="addContactData.address"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="Postcode"
+                                        v-model="addContactData.postcode"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="City"
+                                        v-model="addContactData.city"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        label="Country"
+                                        v-model="addContactData.country"
+                                    ></v-text-field>
+                                    <v-checkbox
+                                        label="Private"
+                                        v-model="addContactData.private"
+                                    ></v-checkbox>
+                                    <v-btn @click="addContact(addContactData)">Save</v-btn>
                                 </v-form>
                             </v-flex>
                         </v-card-text>
@@ -67,7 +126,7 @@
                         <v-list-tile
                             v-for="contact in contacts"
                             :key="contact.title"
-                            @click="editContact(contact)"
+                            @click="showEditContactDialog(contact)"
                         >
                             <v-list-tile-action>
                             <v-icon>contact_mail</v-icon>
@@ -153,12 +212,13 @@
             this.map.addLayers([this.layer_mapnik, this.layer_markers]);
             this.jumpTo(lon, lat, zoom);
 
-            //this.addMarker(this.layer_markers, 13.413215, 52.521918, popuptext);
+            //Add Coordinates to map
+            this.updateLocationMarkers()
         },
         data: () => ({
             contacts: [
-                { name: "Steven B.", address: "Rosa-luxemburg-Strasse 17", postcode: "10178", city: "Berlin", country: "Germany", private: false},
-                { name: "Lincoln M.", address: "Buchholzer Str. 10", postcode: "10437", city: "Berlin", country: "Germany", private: false}
+                { id: 1, name: "Steven B.", address: "Rosa-luxemburg-Strasse 17", postcode: "10178", city: "Berlin", country: "Germany", private: false, longitude: '', latitude: ''},
+                { id: 2, name: "Lincoln M.", address: "Buchholzer Str. 10", postcode: "10437", city: "Berlin", country: "Germany", private: false, longitude: '', latitude: ''}
             ],
             map: '',
             layer_mapnik: '',
@@ -166,10 +226,36 @@
             layer_markers: '',
             errorDialog: false,
             errorText: '',
+            editContactDialog: false,
             addContactDialog: false,
-            contactDialogData: {}
+            contactDialogData: {},
+            addContactData: {}
         }),
         methods: {
+            addContactToMap(contact) {
+                this.addMarker(
+                    this.layer_markers,
+                    contact.longitude,
+                    contact.latitude, 
+                    `<font color=\"black\"><b>${contact.name}<br>${contact.address}<br>${contact.postcode}<br>${contact.city}<br>${contact.country}</b></font>`,
+                    false
+                )
+            },
+            getContactGeoLocation(contact) {
+                return new Promise((resolve, reject) => {
+                    axios.get(`https://nominatim.openstreetmap.org/search?format=json&street=${contact.address}&city=${contact.city}&country=${contact.country}&postalcode=${contact.postalcode}`)
+                    .then((response) => {
+                        let lon = parseFloat(response.data[0].lon)
+                        let lat = parseFloat(response.data[0].lat)
+                        resolve({ lon, lat })
+                    })
+                    .catch((error) => {
+                        reject(error)
+                    })
+                })
+
+
+            },
             jumpTo(lon, lat, zoom) {
                 let x = this.Lon2Merc(lon);
                 let y = this.Lat2Merc(lat);
@@ -184,7 +270,7 @@
                 lat = Math.log(Math.tan( (90 + lat) * PI / 360)) / (PI / 180);
                 return 20037508.34 * lat / 180;
             },
-            addMarker(layer, lon, lat, popupContentHTML) {
+            addMarker(layer, lon, lat, popupContentHTML, showPopup) {
 
                 let ll = new window.OpenLayers.LonLat(this.Lon2Merc(lon), this.Lat2Merc(lat));
                 let feature = new window.OpenLayers.Feature(layer, ll);
@@ -209,7 +295,12 @@
                 marker.events.register("mousedown", feature, markerClick);
 
                 layer.addMarker(marker);
-                this.map.addPopup(feature.createPopup(feature.closeBox));
+                let popup = feature.createPopup(feature.closeBox)
+                this.map.addPopup(popup);
+
+                if(!showPopup) {
+                    popup.hide();
+                }
             },
             getCycleTileURL(bounds) {
                 let res = this.map.getResolution();
@@ -228,19 +319,88 @@
                     return this.url + z + "/" + x + "/" + y + "." + this.type;
                 }
             },
-            editContact(contact) {
+            showEditContactDialog(contact) {
                 axios.get(`https://nominatim.openstreetmap.org/search?format=json&street=${contact.address}&city=${contact.city}&country=${contact.country}&postalcode=${contact.postalcode}`)
                 .then((response) => {
                     console.log(response.data)
                     let longitude = parseFloat(response.data[0].lon)
                     let latitude = parseFloat(response.data[0].lat)
-                    this.jumpTo(longitude, latitude, 16)
-                    this.contactDialogData = contact
-                    this.addContactDialog = true
+                    this.contactDialogData = JSON.parse(JSON.stringify(contact)) //Duplicate vue Data
+                    this.contactDialogData.ref = contact //Save reference to original contact
+                    this.editContactDialog = true
                 })
                 .catch((error) => {
+                    console.log(error)
                     this.errorText = error
+                    this.errorDialog = true
                 })
+            },
+            showErrorDialog(error) {
+                console.log(error)
+                this.errorText = error
+                this.errorDialog = true
+            },
+            updateContact(contact) {
+                contact.ref.name = contact.name
+                contact.ref.address = contact.address
+                contact.ref.city = contact.city
+                contact.ref.postcode = contact.postcode
+                contact.ref.private = contact.private
+                contact.ref.country = contact.country
+                this.editContactDialog = false
+
+                //Update Coordinates
+                this.getContactGeoLocation(contact.ref)
+                .then((location) => {
+                    contact.ref.longitude = location.lon
+                    contact.ref.latitude = location.lat
+                    this.clearLocationMarkers()
+                    this.updateLocationMarkers()
+                })
+                .catch((error) => {
+                    this.showErrorDialog(error)
+                })
+            },
+            updateLocationMarkers() {
+                return new Promise((resolve, reject) => {
+                    this.contacts.forEach((contact) => {
+                        this.getContactGeoLocation(contact)
+                        .then((location) => {
+                            contact.longitude = location.lon
+                            contact.latitude = location.lat
+                            this.addContactToMap(contact)
+                            resolve()
+                        })
+                        .catch((error) => {
+                            this.showErrorDialog(error)
+                        })}
+                    )
+                })
+            },
+            clearLocationMarkers() {
+                this.layer_markers.clearMarkers()
+            },
+            deleteContact(contact) {
+                this.contacts = this.contacts.filter((item) => {
+                    if(item.id !== contact.id) {
+                        console.log(item.name)
+                        return item
+                    }
+                })
+                this.$delete(this.contacts, contact.id)
+                this.clearLocationMarkers()
+                this.updateLocationMarkers().then(() => {
+                    this.editContactDialog = false
+                })
+                
+            },
+            addContact(contact) {
+                this.contacts.push(contact)
+                this.clearLocationMarkers()
+                this.updateLocationMarkers().then(() => {
+                    this.addContactDialog = false
+                })
+                
             }
         }
     }
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Authors: Steven Bradley, Lincoln
 
 Please read on how to use the projects in their respective folders:
 
-- [AddressViz](https://github.com/Steven1811/WADWise18/blob/master/AddressViz/README.md)
-- [findmefinder](https://github.com/Steven1811/WADWise18/blob/master/findmefinder/README.md)
+- [AddressViz](https://github.com/Steven1811/WADWise18/tree/master/AddressViz)
+- [findmefinder](https://github.com/Steven1811/WADWise18/tree/master/findmefinder)
 
